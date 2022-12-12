f486c89c6443c04e6a77115104208228c62a7f6f
andAndrew <andAndrew.k@gmail.com>
2022-12-12 11:50:03 +0400

    feat: begined loginPage

--- src/features/auth/login/LoginPage.tsx
+++ src/features/auth/login/LoginPage.tsx
@@ -1,3 +1,22 @@
+import { Button } from '../../../common/components/button/Button'
+import { Input } from '../../../common/components/input/Input'
+
 export const LoginPage = () => {
-  return <div>Login</div>
+  return (
+    <div>
+      <form>
+        <h2>Sign in</h2>
+        <Input title={''} callBack={() => {}} />
+        <Input title={''} callBack={() => {}} />
+        <span>
+          <input type={'checkbox'} />
+        </span>
+        <span>Remember me</span>
+        <a href={'/passRecovery'}>Forgot Password?</a>
+        <Button title={'Sing In'} onClick={() => {}} />
+        <div>Already have an account?</div>
+        <a href={'/passRecovery'}>Sing In</a>
+      </form>
+    </div>
+  )
 }
