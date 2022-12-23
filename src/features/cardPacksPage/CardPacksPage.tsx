289a49523420dda3463c1bcb1642995757062e3e
andAndrew <andAndrew.k@gmail.com>
2022-12-23 16:13:58 +0400

    feat: merged cardPacksPage with pagination and filtering

--- src/features/cardPacksPage/CardPacksPage.tsx
+++ src/features/cardPacksPage/CardPacksPage.tsx
@@ -124,15 +124,34 @@ export const CardPacksPage = () => {
           Add new pack
         </Button>
       </div>
-      <div className={styles.filterBlock}></div>
+      <div className={styles.filterBlock}>
+        <Filters />
+      </div>
       <div className={styles.table}>
         <TableContainer sx={{ maxHeight: 490 }} component={Paper}>
           <Table aria-label="simple table">
             <TableHead>
               <TableRow>
-                <StyledTableCell align="center">Name</StyledTableCell>
+                <StyledTableCell
+                  align="center"
+                  onClick={
+                    sortPacks === '0name' ? () => handleSort('1name') : () => handleSort('0name')
+                  }
+                  sx={{ cursor: 'pointer' }}
+                >
+                  Name
+                </StyledTableCell>
                 <StyledTableCell align="center">Cards</StyledTableCell>
-                <StyledTableCell align="center">Last updated</StyledTableCell>
+                <StyledTableCell
+                  align="center"
+                  onClick={
+                    sortPacks === '0updated'
+                      ? () => handleSort('1updated')
+                      : () => handleSort('0updated')
+                  }
+                >
+                  Last updated
+                </StyledTableCell>
                 <StyledTableCell align="center">Created by</StyledTableCell>
                 <StyledTableCell align="center">Actions</StyledTableCell>
               </TableRow>
@@ -173,86 +192,270 @@ export const CardPacksPage = () => {
             </TableBody>
           </Table>
         </TableContainer>
-      </div>
-      <Filters />
-      <Button variant={'contained'}>add pack</Button>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell
-                onClick={
-                  sortPacks === '0name' ? () => handleSort('1name') : () => handleSort('0name')
-                }
-                sx={{ cursor: 'pointer' }}
-              >
-                Name
-              </TableCell>
-              <TableCell align="center">Cards</TableCell>
-              <TableCell
-                onClick={
-                  sortPacks === '0updated'
-                    ? () => handleSort('1updated')
-                    : () => handleSort('0updated')
-                }
-                sx={{ cursor: 'pointer' }}
-                align="center"
-              >
-                Last updated
-              </TableCell>
-              <TableCell align="center">Created by</TableCell>
-              <TableCell align="center">Actions</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {packs.map(pack => (
-              <TableRow key={pack._id}>
-                <TableCell component="th" scope="row">
-                  <button onClick={() => {}}>{pack.name}</button>
-                </TableCell>
-                <TableCell align="center">{pack.cardsCount}</TableCell>
-                <TableCell align="center">{pack.updated}</TableCell>
-                <TableCell align="center">{pack.user_name}</TableCell>
-                <TableCell align={'center'}>
-                  <div>
-                    <IconButton onClick={() => {}}>
-                      <SchoolOutlinedIcon />
-                    </IconButton>
-                    <IconButton onClick={() => {}}>
-                      <DriveFileRenameOutlineIcon />
-                    </IconButton>
-                    <IconButton onClick={() => {}}>
-                      <DeleteOutline />
-                    </IconButton>
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <div className={style.PaginationContainer}>
-        <ReactPaginate
-          pageCount={Math.ceil(cardPacksTotalCount / pageCount)}
-          nextLabel={'>'}
-          breakLabel={'...'}
-          previousLabel={'<'}
-          containerClassName={style.paginationBttns}
-          disabledClassName={style.paginationDisabled}
-          activeClassName={style.paginationActive}
-          onPageChange={handlePageClick}
-        />
-        <div className={style.SelectContainer}>
-          Show
-          <NativeSelect onChange={handlePageCountChange} sx={{ margin: '5px' }} defaultValue={10}>
-            <option value={10}>10</option>
-            <option value={15}>15</option>
-            <option value={50}>50</option>
-            <option value={100}>100</option>
-          </NativeSelect>
-          Cards per page
+        <div className={style.PaginationContainer}>
+          <ReactPaginate
+            pageCount={Math.ceil(cardPacksTotalCount / pageCount)}
+            nextLabel={'>'}
+            breakLabel={'...'}
+            previousLabel={'<'}
+            containerClassName={style.paginationBttns}
+            disabledClassName={style.paginationDisabled}
+            activeClassName={style.paginationActive}
+            onPageChange={handlePageClick}
+          />
+          <div className={style.SelectContainer}>
+            Show
+            <NativeSelect onChange={handlePageCountChange} sx={{ margin: '5px' }} defaultValue={10}>
+              <option value={10}>10</option>
+              <option value={15}>15</option>
+              <option value={50}>50</option>
+              <option value={100}>100</option>
+            </NativeSelect>
+            Cards per page
+          </div>
         </div>
       </div>
     </div>
   )
+}
+
+{
+  /*<Filters />*/
+}
+{
+  /*<Button variant={'contained'}>add pack</Button>*/
+}
+{
+  /*<TableContainer component={Paper}>*/
+}
+{
+  /*  <Table sx={{ minWidth: 650 }} aria-label="simple table">*/
+}
+{
+  /*    <TableHead>*/
+}
+{
+  /*      <TableRow>*/
+}
+{
+  /*        <TableCell*/
+}
+{
+  /*          onClick={*/
+}
+{
+  /*            sortPacks === '0name' ? () => handleSort('1name') : () => handleSort('0name')*/
+}
+{
+  /*          }*/
+}
+{
+  /*          sx={{ cursor: 'pointer' }}*/
+}
+{
+  /*        >*/
+}
+{
+  /*          Name*/
+}
+{
+  /*        </TableCell>*/
+}
+{
+  /*        <TableCell align="center">Cards</TableCell>*/
+}
+{
+  /*        <TableCell*/
+}
+{
+  /*          onClick={*/
+}
+{
+  /*            sortPacks === '0updated'*/
+}
+{
+  /*              ? () => handleSort('1updated')*/
+}
+{
+  /*              : () => handleSort('0updated')*/
+}
+{
+  /*          }*/
+}
+{
+  /*          sx={{ cursor: 'pointer' }}*/
+}
+{
+  /*          align="center"*/
+}
+{
+  /*        >*/
+}
+{
+  /*          Last updated*/
+}
+{
+  /*        </TableCell>*/
+}
+{
+  /*        <TableCell align="center">Created by</TableCell>*/
+}
+{
+  /*        <TableCell align="center">Actions</TableCell>*/
+}
+{
+  /*      </TableRow>*/
+}
+{
+  /*    </TableHead>*/
+}
+{
+  /*    <TableBody>*/
+}
+{
+  /*{packs.map(pack => (*/
+}
+{
+  /*  <TableRow key={pack._id}>*/
+}
+{
+  /*    <TableCell component="th" scope="row">*/
+}
+{
+  /*      <button onClick={() => {}}>{pack.name}</button>*/
+}
+{
+  /*    </TableCell>*/
+}
+{
+  /*    <TableCell align="center">{pack.cardsCount}</TableCell>*/
+}
+{
+  /*    <TableCell align="center">{pack.updated}</TableCell>*/
+}
+{
+  /*    <TableCell align="center">{pack.user_name}</TableCell>*/
+}
+{
+  /*    <TableCell align={'center'}>*/
+}
+{
+  /*      <div>*/
+}
+{
+  /*        <IconButton onClick={() => {}}>*/
+}
+{
+  /*          <SchoolOutlinedIcon />*/
+}
+{
+  /*        </IconButton>*/
+}
+{
+  /*        <IconButton onClick={() => {}}>*/
+}
+{
+  /*            <DriveFileRenameOutlineIcon />*/
+}
+{
+  /*          </IconButton>*/
+}
+{
+  /*          <IconButton onClick={() => {}}>*/
+}
+{
+  /*            <DeleteOutline />*/
+}
+{
+  /*          </IconButton>*/
+}
+{
+  /*        </div>*/
+}
+{
+  /*      </TableCell>*/
+}
+{
+  /*    </TableRow>*/
+}
+{
+  /*  ))}*/
+}
+{
+  /*</TableBody>*/
+}
+{
+  /*  </Table>*/
+}
+{
+  /*</TableContainer>*/
+}
+{
+  /*<div className={style.PaginationContainer}>*/
+}
+{
+  /*  <ReactPaginate*/
+}
+{
+  /*    pageCount={Math.ceil(cardPacksTotalCount / pageCount)}*/
+}
+{
+  /*    nextLabel={'>'}*/
+}
+{
+  /*    breakLabel={'...'}*/
+}
+{
+  /*    previousLabel={'<'}*/
+}
+{
+  /*    containerClassName={style.paginationBttns}*/
+}
+{
+  /*    disabledClassName={style.paginationDisabled}*/
+}
+{
+  /*    activeClassName={style.paginationActive}*/
+}
+{
+  /*    onPageChange={handlePageClick}*/
+}
+{
+  /*  />*/
+}
+{
+  /*  <div className={style.SelectContainer}>*/
+}
+{
+  /*    Show*/
+}
+{
+  /*    <NativeSelect onChange={handlePageCountChange} sx={{ margin: '5px' }} defaultValue={10}>*/
+}
+{
+  /*      <option value={10}>10</option>*/
+}
+{
+  /*      <option value={15}>15</option>*/
+}
+{
+  /*      <option value={50}>50</option>*/
+}
+{
+  /*      <option value={100}>100</option>*/
+}
+{
+  /*    </NativeSelect>*/
+}
+{
+  /*    Cards per page*/
+}
+{
+  /*  </div>*/
+}
+{
+  /*</div>*/
+}
+{
+  /*</div>*/
 }
